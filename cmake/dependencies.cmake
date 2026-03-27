96e7fe3dcd09074c3ddee0182d5b0496ad1bfc84
milanzlicic <milan.zlicic@ready2wash.com>
2026-03-27 11:09:13 +0100

    Use cmake modbus

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,4 +1,4 @@
-include(WolkSDK-Cpp/cpp-tools/cmake/dependencies/libmodbus.cmake)
+include(WolkSDK-Cpp/cpp-tools/cmake/dependencies/libmodbus_android/libmodbus.cmake)
 
 if (MOREMODBUS_BUILD_TESTS)
     include(WolkSDK-Cpp/cpp-tools/cmake/dependencies/gtest.cmake)
